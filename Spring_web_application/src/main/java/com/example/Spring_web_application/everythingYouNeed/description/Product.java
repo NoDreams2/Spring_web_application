48a0f39586d36b97c5aaca812fb7091085e00a8c
NoDreams2 <lykyanov2005@mail.ru>
2024-09-08 11:38:55 +0700

    добавил описание товара

--- Spring_web_application/src/main/java/com/example/Spring_web_application/everythingYouNeed/description/Product.java
+++ Spring_web_application/src/main/java/com/example/Spring_web_application/everythingYouNeed/description/Product.java
@@ -1,4 +1,17 @@
 package com.example.Spring_web_application.everythingYouNeed.description;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Product {
+
+  private Integer id;
+
+  private String title;
+
+  private String details;
 }
